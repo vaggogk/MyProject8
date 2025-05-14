84ea76ad1f2b322d6d5bc0cc263d61c773876f75
vaggogk <vaggogk@gmail.com>
2025-05-14 22:51:51 +0300

    feat:add print formatted by main branch

--- src/gr/aueb/ch/helloapp/AddApp.java
+++ src/gr/aueb/ch/helloapp/AddApp.java
@@ -4,6 +4,7 @@ public class AddApp {
 
     public static void main(String[] args) {
         System.out.println(add(3,5));
+        System.out.printf("%4d", add(7,2));
     }
 
     public static int add (int a, int b){
